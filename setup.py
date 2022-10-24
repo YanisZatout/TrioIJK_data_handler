2afc7225d0aa2c831098e83e3a5123c11e1bb13f
Yanis ZATOUT <yanis.zatout@universite-paris-saclay.fr>
2022-10-24 11:15:04 +0200

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='TrioIJK_data_handler',
+    version='0.1',
+    description='IJK simulations dataloader and visualisation tool',
+    author='Yanis Zatout',
+    author_email='yanis.zatout@cnrs.fr',
+    url='https://github.com/YanisZatout/TrioIJK_data_handler',
+    packages=['distutils', 'distutils.command'],
+)
