41feab5d213f4f2d9cd17602cbd06099f9d6b68e
Yanis ZATOUT <yanis.zatout@universite-paris-saclay.fr>
2023-02-27 17:32:07 +0100

    Adding relevent functions to init

--- datahandling/__init__.py
+++ datahandling/__init__.py
@@ -1,3 +1,11 @@
-from .dataloader import DataLoader, DataLoaderPandas
-from .dataplotter import matplotlib_latex_params
-from .error_quantification import compute_eps, assess_mean_error_across_quantities
+from .dataformatter import format_model
+from .dataloader import (DataLoader,
+                         DataLoaderPandas)
+from .dataplotter import (matplotlib_latex_params,
+                          verif_convergence_irene_les)
+from .error_quantification import (compute_eps,
+                                   assess_mean_error_across_quantities,
+                                   compute_rms,
+                                   friction_quantities_les)
+from .post_treatement import (generate_formulas,)
+from .quantities_of_interest import compute_rms_quantities
