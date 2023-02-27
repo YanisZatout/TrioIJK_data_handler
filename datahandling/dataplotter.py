f90438d31edf13ab1346ed14dac19fd40c0947dd
Yanis ZATOUT <yanis.zatout@universite-paris-saclay.fr>
2023-02-27 17:08:55 +0100

    Adding functions to verify statistics convergence
    compute_Rij_middle_canal:
      Computes the diagonal terms of the Reynolds'
      tensor. Not particularly useful.
    compute_uu_middle_canal:
      Gets the velocity and RMS of the streamwise
      velocity in the middle of the canal.
      Useful in visualising a simulation's convergence.
    compute_T_middle_canal:
      Gets the temperature and RMS of the temperature
      in the middle of the canal.
      Useful in visualising a simulation's convergence.
    verify_convergence:
      Plotting function to get the above mentioned
      quantities easily.
    verif_convergence_irene_les:
      Specific function to automatically verify and
      write a PDF plot to the disk.
      Very useful when using a computational cluster.

--- datahandling/dataplotter.py
+++ datahandling/dataplotter.py
@@ -1,5 +1,6 @@
 from typing import Tuple, Union
 
+import re
 import pandas as pd
 from datahandling.dataloader import DataLoader, DataLoaderPandas
 import numpy as np
@@ -76,9 +77,9 @@ def compute_Rij_middle_canal(loader: DataLoaderPandas, df: pd.DataFrame):
 def compute_uu_middle_canal(loader: DataLoaderPandas, df: pd.DataFrame):
     middle_canal = len(loader.space)//2
     newdf = pd.DataFrame()
+    newdf["U"] = df["U"].groupby("time").apply(lambda x: x.iloc[middle_canal])
     newdf["U'U'"] = (df["UU"] - df["U"] **
                      2).groupby("time").apply(lambda x: x.iloc[middle_canal])
-    newdf["U"] = df["U"].groupby("time").apply(lambda x: x.iloc[middle_canal])
     return newdf
 
 
@@ -106,7 +107,6 @@ def verify_convergence(rep: str = "./"):
     plt.grid(True)
     fig.suptitle(r"$U$ and $\langle U' U' \rangle$ in middle of canal")
 
-
     fig, axs = plt.subplots(1, len(T.columns), figsize=(16, 9))
     for c, ax in zip(T.columns, axs):
         T[c].plot(ylabel=c, ax=ax)
@@ -114,3 +114,72 @@ def verify_convergence(rep: str = "./"):
     fig.suptitle(r"$T$ and $\langle T' T' \rangle$ in middle of canal")
 
     plt.show()
+
+
+def verif_convergence_irene_les(rep: str = "./"):
+    import pandas as pd
+    matplotlib_latex_params()
+    loader = DataLoaderPandas(rep, type_stat="statistiques")
+    df: pd.DataFrame = loader.load_data()
+    uu = compute_uu_middle_canal(loader, df)
+    T = compute_T_middle_canal(loader, df)
+    import os
+    home_directory = os.path.expanduser('~')
+    # all = pd.concat([uu, T], axis=1)
+    fig, axs = plt.subplots(1, len(uu.columns), figsize=(16, 9))
+    for c, ax in zip(uu.columns, axs):
+        uu[c].plot(ylabel=c, ax=ax)
+    plt.grid(True)
+    type_sim, _, models, mesh = os.getcwd().split("/")[-4:]
+    turb_visc_diff, disc_qdm, disc_rho = models.split("_")
+    discretisation_qdm, discretisation_rho = disc_qdm, disc_rho
+    turb_visc, turb_diff = turb_visc_diff.split("-")
+    turb_visc_models, turb_diff_models = turb_visc.split(
+        "+"), turb_diff.split("+")
+
+    turb_visc_constants, turb_diff_constants = re.findall(
+        r"\d+", turb_visc), re.findall(r"\d+", turb_diff)
+
+    turb_visc_constants_corrected = ["0."+x[1:]
+                                     for x in turb_visc_constants if x.startswith("0")]
+    turb_diff_constants_corrected = ["0."+x[1:]
+                                     for x in turb_diff_constants if x.startswith("0")]
+
+    tau_string = ""
+    for tau_constant, tau_bad_constant, tau_model in zip(turb_visc_constants_corrected, turb_visc_constants, turb_visc_models):
+        tau_model = tau_model.split(tau_bad_constant)[0]
+        tau_string += f"{tau_constant}" + r"\tau^{" + f"{tau_model}" + r"}"
+        if len(turb_visc_constants_corrected) > 1:
+            tau_string += "+"
+
+    pi_string = ""
+    for pi_constant, pi_bad_constant, pi_model in zip(turb_diff_constants_corrected, turb_diff_constants, turb_diff_models):
+        pi_model = pi_model.split(pi_bad_constant)[0]
+        pi_string += f"{pi_constant}" + r"\pi^{" + f"{pi_model}" + r"}"
+        if len(turb_visc_constants_corrected) > 1:
+            pi_string += "+"
+    fig.suptitle(
+        r"$\langle U \rangle$, $\langle U' U' \rangle$ middle canal for  $\tau_{ij}=" +
+        tau_string + r"$, $\pi_j=" + pi_string + "$"
+        f"qdm: {discretisation_qdm}, rho: {discretisation_rho}, mesh: {mesh}")
+    os.makedirs(os.path.join(home_directory,
+                             "simulation_advancement"), exist_ok=True)
+    plt.savefig(os.path.join(home_directory,
+                "simulation_advancement", f"{models}_{mesh}_velocity.pdf"), dpi=150)
+
+    fig, axs = plt.subplots(1, len(T.columns), figsize=(16, 9))
+    for c, ax in zip(T.columns, axs):
+        T[c].plot(ylabel=c, ax=ax)
+    plt.grid(True)
+    fig.suptitle(
+        r"$\langle T \rangle$ and $\langle T' T' \rangle$ $\tau_{ij}=" +
+        tau_string + r"$, $\pi_j=" + pi_string + "$"
+
+        f"qdm: {discretisation_qdm}, rho: {discretisation_rho}, mesh: {mesh}")
+    plt.savefig(
+        os.path.join(home_directory,
+                     "simulation_advancement", f"{models}_{mesh}_temperature.pdf"), dpi=150
+
+    )
+
+    # plt.show()
