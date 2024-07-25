196983d2e77a864250efd36e01ec38f1cd9b7fe5
Yanis ZATOUT <yanis.zatout@universite-paris-saclay.fr>
2024-07-25 16:42:12 +0200

    Changes to RMS computing function
    In function responsible for computing
    rms quantities relative to LES, corrected
    mistakes relative to second order
    statistics formulas

--- datahandling/quantities_of_interest.py
+++ datahandling/quantities_of_interest.py
@@ -51,80 +51,100 @@ def compute_rms_quantities(df_les: List[pd.DataFrame], df_dns: pd.DataFrame, Cp=
     \langle T' T^{'} \rangle
     """
     urms_les = [
-        df["UU"] - df["U"]**2 - df["STRUCTURAL_UU"]/df["RHO"]
+        df["UU"] - df["U"]**2 - 1/3 * (df["UU"] - df["U"]**2 + df["VV"] - df["V"]**2 + df["WW"] - df["W"]**2)
+        + df["STRUCTURAL_UU"]
         - 2 * df["NUTURB_XX_DUDX"]
-        + 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] +
-                 df["STRUCTURAL_WW"])/df["RHO"]
-        + 2/3 * (df["NUTURB_XX_DUDX"] +
-                 df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"])
+        - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])
+        + 2/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"])
         for df in df_les
     ]
-    urms_dns = df_dns["UU"] - df_dns["U"]**2 \
-        - df_dns["STRUCTURAL_UU"]/df_dns["RHO"] \
-        - 2 * df_dns["NUTURB_XX_DUDX"] \
-        + 1/3 * (df_dns["STRUCTURAL_UU"] + df_dns["STRUCTURAL_VV"] + df_dns["STRUCTURAL_WW"])/df_dns["RHO"] \
-        + 2/3 * (df_dns["NUTURB_XX_DUDX"] +
-                 df_dns["NUTURB_YY_DVDY"] + df_dns["NUTURB_ZZ_DWDZ"])
+    urms_dns = df_dns["UU"] - df_dns["U"]**2 - 1/3 * (df_dns["UU"] - df_dns["U"]**2 + df_dns["VV"] - df_dns["V"]**2 + df_dns["WW"] - df_dns["W"]**2))
+
     vrms_les = [
-        df["WW"] - df["W"]**2
-        - 1/3 * (df["UU"] - df["U"]**2 + df["VV"] -
-                 df["V"]**2 + df["WW"] - df["W"]**2)
-        - df["STRUCTURAL_WW"]/df["RHO"]
+        df["WW"] - df["W"]**2 - 1/3 * (df["UU"] - df["U"]**2 + df["VV"] - df["V"]**2 + df["WW"] - df["W"]**2)
+        + df["STRUCTURAL_WW"] 
         - 2 * df["NUTURB_ZZ_DWDZ"]
-        + 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] +
-                 df["STRUCTURAL_WW"])/df["RHO"]
-        + 2/3 * (df["NUTURB_XX_DUDX"] +
-                 df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"])
+        - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])
+        + 2/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"])
         for df in df_les
     ]
-    vrms_dns = (
-        df_dns["WW"] - df_dns["W"]**2
-        - 1/3 * (df_dns["UU"] - df_dns["U"]**2 + df_dns["VV"] -
-                 df_dns["V"]**2 + df_dns["WW"] - df_dns["W"]**2)
-        - df_dns["STRUCTURAL_WW"]/df_dns["RHO"]
-        - 2 * df_dns["NUTURB_ZZ_DWDZ"]
-        + 1/3 * (df_dns["STRUCTURAL_UU"] + df_dns["STRUCTURAL_VV"] +
-                 df_dns["STRUCTURAL_WW"])/df_dns["RHO"]
-        + 2/3 * (df_dns["NUTURB_XX_DUDX"] + df_dns["NUTURB_YY_DVDY"] + df_dns["NUTURB_ZZ_DWDZ"]))
+    vrms_dns = ( df_dns["WW"] - df_dns["W"]**2 - 1/3 * (df_dns["UU"] - df_dns["U"]**2 + df_dns["VV"] - df_dns["V"]**2 + df_dns["WW"] - df_dns["W"]**2))
     wrms_les = [
-        df["V"] - df["V"]**2
-        - 1/3 * (df["UU"] - df["U"]**2 + df["VV"] -
-                 df["V"]**2 + df["WW"] - df["W"]**2)
-        - df["STRUCTURAL_VV"]/df["RHO"]
+        df["VV"] - df["V"]**2 -  1/3 * (df["UU"] - df["U"]**2 + df["VV"] - df["V"]**2 + df["WW"] - df["W"]**2)
+        + df["STRUCTURAL_VV"]
         - 2 * df["NUTURB_YY_DVDY"]
-        + 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] +
-                 df["STRUCTURAL_WW"])/df["RHO"]
+        - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] +
+                 df["STRUCTURAL_WW"])
         + 2/3 * (df["NUTURB_XX_DUDX"] +
                  df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"])
         for df in df_les
     ]
-    wrms_dns = (
-        df_dns["V"] - df_dns["V"]**2
-        - 1/3 * (df_dns["UU"] - df_dns["U"]**2 + df_dns["VV"] -
-                 df_dns["V"]**2 + df_dns["WW"] - df_dns["W"]**2)
-        - df_dns["STRUCTURAL_VV"]/df_dns["RHO"]
-        - 2 * df_dns["NUTURB_YY_DVDY"]
-        + 1/3 * (df_dns["STRUCTURAL_UU"] + df_dns["STRUCTURAL_VV"] +
-                 df_dns["STRUCTURAL_WW"])/df_dns["RHO"]
-        + 2/3 * (df_dns["NUTURB_XX_DUDX"] + df_dns["NUTURB_YY_DVDY"] + df_dns["NUTURB_ZZ_DWDZ"]))
+    wrms_dns = (df_dns["VV"] - df_dns["V"]**2 1/3 * (df_dns["UU"] - df_dns["U"]**2 + df_dns["VV"] - df_dns["V"]**2 + df_dns["WW"] - df_dns["W"]**2))
 
-    uv_les = [(df["UW"]-df["U"]*df["W"]-df["STRUCTURAL_UW"]/df["RHO"] -
-               (df["NUTURB_XZ_DUDZ"]+df["NUTURB_XZ_DWDX"])) for df in df_les]
-    uv_dns = df_dns["UW"]-df_dns["U"]*df_dns["W"]-df_dns["STRUCTURAL_UW"] / \
-        df_dns["RHO"]-(df_dns["NUTURB_XZ_DUDZ"]+df_dns["NUTURB_XZ_DWDX"])
+    uv_les = [(df["UW"]-df["U"]*df["W"] + df["STRUCTURAL_UW"] - (df["NUTURB_XZ_DUDZ"]+df["NUTURB_XZ_DWDX"])) for df in df_les]
+    uv_dns = df_dns["UW"]-df_dns["U"]*df_dns["W"]
 
-    u_theta_les = [(df["UT"] - df["U"] * df["T"] - df["KAPPATURB_X_DSCALARDX"] *
-                    df["RHO"]*Cp - df["STRUCTURAL_USCALAR"] / df["T"] / df["RHO"]) for df in df_les]
-    u_theta_dns = (df_dns["UT"]-df_dns["U"]*df_dns["T"]-df_dns["KAPPATURB_X_DSCALARDX"]
-                   * df_dns["RHO"]*Cp-df_dns["STRUCTURAL_USCALAR"]/df_dns["T"]/df_dns["RHO"])
+    u_theta_les = [(df["UT"] - df["U"] * df["T"] - 2* df["KAPPATURB_X_DSCALARDX"]  + df["STRUCTURAL_USCALAR"]) for df in df_les]
+    u_theta_dns = (df_dns["UT"]-df_dns["U"]*df_dns["T"])
 
-    v_theta_les = [(df["WT"] - df["W"] * df["T"] - df["KAPPATURB_Z_DSCALARDZ"] *
-                    df["RHO"]*Cp - df["STRUCTURAL_WSCALAR"] / df["RHO"] / df["T"]) for df in df_les]
-    v_theta_dns = df_dns["WT"]-df_dns["W"]*df_dns["T"]-df_dns["KAPPATURB_Z_DSCALARDZ"] * \
-        df_dns["RHO"]*Cp-df_dns["STRUCTURAL_WSCALAR"]/df_dns["RHO"]/df_dns["T"]
+    v_theta_les = [(df["WT"] - df["W"] * df["T"] - 2*df["KAPPATURB_Z_DSCALARDZ"] + df["STRUCTURAL_WSCALAR"]) for df in df_les]
+    v_theta_dns = df_dns["WT"]-df_dns["W"]*df_dns["T"]
 
-    theta_rms_les = [(sqrt(df["T2"]-df["T"]*df["T"])) for df in df_les]
-    theta_rms_dns = (sqrt(df_dns["T2"]-df_dns["T"]*df_dns["T"]))
+    theta_rms_les = [((df["T2"]-df["T"]*df["T"])) for df in df_les]
+    theta_rms_dns = ((df_dns["T2"]-df_dns["T"]*df_dns["T"]))
+    les = {"urms": urms_les, "vrms": vrms_les, "wrms": wrms_les, "uv": uv_les, "u_theta": u_theta_les, "v_theta": v_theta_les, "theta_rms": theta_rms_les}
+    dns = {"urms": urms_dns, "vrms": vrms_dns, "wrms": wrms_dns, "uv": uv_dns, "u_theta": u_theta_dns, "v_theta": v_theta_dns, "theta_rms": theta_rms_dns}
+
+    return les, dns
+
+
+def compute_rms_quantities_compressible(df_les: List[pd.DataFrame], df_dns: pd.DataFrame, Cp=1155) -> Tuple[Dict]:
+    """
+    Computes RMS quantities of interest for compressible structural models:
+    \langle u^{'2} \rangle ^{dev}
+    \langle v^{'2} \rangle ^{dev}
+    \langle w^{'2} \rangle ^{dev}
+
+    \langle u' v^{'} \rangle
+
+    \langle u' T^{'} \rangle
+    \langle v' T^{'} \rangle
+    \langle T' T^{'} \rangle
+    """
+    urms_les = [
+        df["UU"] - df["U"]**2 - 1/3 * (df["UU"] - df["U"]**2 + df["VV"] - df["V"]**2 + df["WW"] - df["W"]**2)
+        + df["STRUCTURAL_UU"]/df["RHO"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])/df["RHO"]
+        - 2 * (df["NUTURB_XX_DUDX"] - 1/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"]))
+        for df in df_les
+    ]
+    urms_dns = df_dns["UU"] - df_dns["U"]**2 - 1/3 * (df_dns["UU"] - df_dns["U"]**2 + df_dns["VV"] - df_dns["V"]**2 + df_dns["WW"] - df_dns["W"]**2))
+
+    vrms_les = [
+        df["WW"] - df["W"]**2 - 1/3 * (df["UU"] - df["U"]**2 + df["VV"] - df["V"]**2 + df["WW"] - df["W"]**2)
+        + df["STRUCTURAL_WW"] /df["RHO"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])/df["RHO"]
+        - 2 * ( df["NUTURB_ZZ_DWDZ"] - 1/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"]))
+        for df in df_les
+    ]
+    vrms_dns = ( df_dns["WW"] - df_dns["W"]**2 - 1/3 * (df_dns["UU"] - df_dns["U"]**2 + df_dns["VV"] - df_dns["V"]**2 + df_dns["WW"] - df_dns["W"]**2))
+    wrms_les = [
+        df["VV"] - df["V"]**2 -  1/3 * (df["UU"] - df["U"]**2 + df["VV"] - df["V"]**2 + df["WW"] - df["W"]**2)
+        + df["STRUCTURAL_VV"]/df["RHO"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])/df["RHO"]
+        - 2 *(df["NUTURB_YY_DVDY"] - 1/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"]))
+        for df in df_les
+    ]
+    wrms_dns = (df_dns["VV"] - df_dns["V"]**2  - 1/3 * (df_dns["UU"] - df_dns["U"]**2 + df_dns["VV"] - df_dns["V"]**2 + df_dns["WW"] - df_dns["W"]**2))
+
+    uv_les = [(df["UW"]-df["U"]*df["W"] + df["STRUCTURAL_UW"]/df["RHO"] - (df["NUTURB_XZ_DUDZ"]+df["NUTURB_XZ_DWDX"])) for df in df_les]
+    uv_dns = df_dns["UW"] - df_dns["U"] * df_dns["W"]
+
+    u_theta_les = [(df["UT"] - df["U"] * df["T"] - 2* df["KAPPATURB_X_DSCALARDX"] + df["STRUCTURAL_USCALAR"]/df["RHO"]) for df in df_les]
+    u_theta_dns = (df_dns["UT"]-df_dns["U"]*df_dns["T"])
+
+    v_theta_les = [(df["WT"] - df["W"] * df["T"] - 2*df["KAPPATURB_Z_DSCALARDZ"] + df["STRUCTURAL_WSCALAR"]/df["RHO"]) for df in df_les]
+    v_theta_dns = df_dns["WT"]-df_dns["W"]*df_dns["T"]
+
+    theta_rms_les = [((df["T2"]-df["T"]*df["T"])) for df in df_les]
+    theta_rms_dns = ((df_dns["T2"]-df_dns["T"]*df_dns["T"]))
     les = {"urms": urms_les, "vrms": vrms_les, "wrms": wrms_les, "uv": uv_les, "u_theta": u_theta_les, "v_theta": v_theta_les, "theta_rms": theta_rms_les}
     dns = {"urms": urms_dns, "vrms": vrms_dns, "wrms": wrms_dns, "uv": uv_dns, "u_theta": u_theta_dns, "v_theta": v_theta_dns, "theta_rms": theta_rms_dns}
 
