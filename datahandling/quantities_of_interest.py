e7fb1648d7bd2cc54e85ee7af475c026b243b052
Yanis ZATOUT <yanis.zatout@universite-paris-saclay.fr>
2024-07-29 17:32:18 +0200

    Added functions to compute rms and closure
    Added functions rms to compute the rms
    quantities of the simulation.
    Added function closure_terms to compute
    closure terms of each LES done.
    Added function adim_closure_terms to
    nondimentionalize simulation quantities

--- datahandling/quantities_of_interest.py
+++ datahandling/quantities_of_interest.py
@@ -2,6 +2,8 @@ import numpy as np
 from typing import Dict, List, Tuple, Union
 import pandas as pd
 
+from datahandling.oscillation import RefData
+
 
 def adim_qqty(
     df_les: list[pd.DataFrame],
@@ -269,6 +271,180 @@ def compute_rms_quantities_compressible(df_les: List[pd.DataFrame], df_dns: pd.D
               "wrms_dev":wrms_fonc_dev , "uv": uv_fonc, "u_theta": u_theta_fonc, "v_theta": v_theta_fonc}
 
     return les, dns, struct, fonc
+
+
+def rms(df_les: List[pd.DataFrame], df_dns: pd.DataFrame, Cp=1155, tau_compressible=False, pi_compressible=False) -> Tuple[Dict]:
+    """
+    Computes RMS quantities of interest for compressible structural models:
+    \langle u^{'2} \rangle ^{dev}
+    \langle v^{'2} \rangle ^{dev}
+    \langle w^{'2} \rangle ^{dev}
+
+    \langle u' v^{'} \rangle
+
+    \langle u' T^{'} \rangle
+    \langle v' T^{'} \rangle
+    \langle T' T^{'} \rangle
+    """
+    if tau_compressible:
+        urms_les = [
+            df["UU"] - df["U"]**2 - 1/3 * (df["UU"] - df["U"]**2 + df["VV"] - df["V"]**2 + df["WW"] - df["W"]**2)
+            + df["STRUCTURAL_UU"]/df["RHO"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])/df["RHO"]
+            - 2 * (df["NUTURB_XX_DUDX"] - 1/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"]))
+            for df in df_les
+        ]
+
+        vrms_les = [
+            df["WW"] - df["W"]**2 - 1/3 * (df["UU"] - df["U"]**2 + df["VV"] - df["V"]**2 + df["WW"] - df["W"]**2)
+            + df["STRUCTURAL_WW"] /df["RHO"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])/df["RHO"]
+            - 2 * ( df["NUTURB_ZZ_DWDZ"] - 1/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"]))
+            for df in df_les
+        ]
+
+        wrms_les = [
+            df["VV"] - df["V"]**2 -  1/3 * (df["UU"] - df["U"]**2 + df["VV"] - df["V"]**2 + df["WW"] - df["W"]**2)
+            + df["STRUCTURAL_VV"]/df["RHO"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])/df["RHO"]
+            - 2 *(df["NUTURB_YY_DVDY"] - 1/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"]))
+            for df in df_les
+        ]
+        uv_les = [(df["UW"]-df["U"]*df["W"] + df["STRUCTURAL_UW"]/df["RHO"] - (df["NUTURB_XZ_DUDZ"]+df["NUTURB_XZ_DWDX"])) for df in df_les]
+    else:
+        urms_les = [
+            df["UU"] - df["U"]**2 - 1/3 * (df["UU"] - df["U"]**2 + df["VV"] - df["V"]**2 + df["WW"] - df["W"]**2)
+            + df["STRUCTURAL_UU"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])
+            - 2 * (df["NUTURB_XX_DUDX"] - 1/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"]))
+            for df in df_les
+        ]
+
+        vrms_les = [
+            df["WW"] - df["W"]**2 - 1/3 * (df["UU"] - df["U"]**2 + df["VV"] - df["V"]**2 + df["WW"] - df["W"]**2)
+            + df["STRUCTURAL_WW"]  - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])
+            - 2 * ( df["NUTURB_ZZ_DWDZ"] - 1/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"]))
+            for df in df_les
+        ]
+
+        wrms_les = [
+            df["VV"] - df["V"]**2 -  1/3 * (df["UU"] - df["U"]**2 + df["VV"] - df["V"]**2 + df["WW"] - df["W"]**2)
+            + df["STRUCTURAL_VV"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])
+            - 2 *(df["NUTURB_YY_DVDY"] - 1/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"]))
+            for df in df_les
+        ]
+        uv_les = [(df["UW"]-df["U"]*df["W"] + df["STRUCTURAL_UW"] - (df["NUTURB_XZ_DUDZ"]+df["NUTURB_XZ_DWDX"])) for df in df_les]
+    if pi_compressible:
+        u_theta_les = [(df["UT"] - df["U"] * df["T"] - 2* df["KAPPATURB_X_DSCALARDX"] + df["STRUCTURAL_USCALAR"]/df["RHO"]) for df in df_les]
+        v_theta_les = [(df["WT"] - df["W"] * df["T"] - 2*df["KAPPATURB_Z_DSCALARDZ"] + df["STRUCTURAL_WSCALAR"]/df["RHO"]) for df in df_les]
+    else:
+        u_theta_les = [(df["UT"] - df["U"] * df["T"] - 2* df["KAPPATURB_X_DSCALARDX"] + df["STRUCTURAL_USCALAR"]) for df in df_les]
+        v_theta_les = [(df["WT"] - df["W"] * df["T"] - 2*df["KAPPATURB_Z_DSCALARDZ"] + df["STRUCTURAL_WSCALAR"]) for df in df_les]
+
+    urms_dns = df_dns["UU"] - df_dns["U"]**2 - 1/3 * (df_dns["UU"] - df_dns["U"]**2 + df_dns["VV"] - df_dns["V"]**2 + df_dns["WW"] - df_dns["W"]**2)
+    vrms_dns = ( df_dns["WW"] - df_dns["W"]**2 - 1/3 * (df_dns["UU"] - df_dns["U"]**2 + df_dns["VV"] - df_dns["V"]**2 + df_dns["WW"] - df_dns["W"]**2))
+    wrms_dns = (df_dns["VV"] - df_dns["V"]**2  - 1/3 * (df_dns["UU"] - df_dns["U"]**2 + df_dns["VV"] - df_dns["V"]**2 + df_dns["WW"] - df_dns["W"]**2))
+    uv_dns = df_dns["UW"] - df_dns["U"] * df_dns["W"]
+    u_theta_dns = (df_dns["UT"]-df_dns["U"]*df_dns["T"])
+    v_theta_dns = df_dns["WT"]-df_dns["W"]*df_dns["T"]
+
+    theta_rms_les = [((df["T2"]-df["T"]*df["T"])) for df in df_les]
+
+    theta_rms_dns = ((df_dns["T2"]-df_dns["T"]*df_dns["T"]))
+
+    les = {"urms": urms_les, "vrms": vrms_les, "wrms": wrms_les, "uv": uv_les, "u_theta": u_theta_les, "v_theta": v_theta_les, "theta_rms": theta_rms_les}
+    dns = {"urms": urms_dns, "vrms": vrms_dns, "wrms": wrms_dns, "uv": uv_dns, "u_theta": u_theta_dns, "v_theta": v_theta_dns, "theta_rms": theta_rms_dns}
+
+    for key, value in les.items():
+        les[key] = np.sqrt(value)
+    for key, value in dns.items():
+        dns[key] = np.sqrt(value)
+
+    return les, dns
+
+
+
+def closure_terms(df_les: List[pd.DataFrame], Cp=1155, tau_compressible=False, pi_compressible=False) -> Tuple[Dict]:
+    """
+    Gets the closure terms for velocity velocity correlations
+    and velocity temperature correlations
+    """
+    # Tau_{x, x}
+    if tau_compressible:
+        urms_struct = [df["STRUCTURAL_UU"]/df["RHO"] for df in df_les]
+        urms_struct_dev = [df["STRUCTURAL_UU"]/df["RHO"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])/df["RHO"] for df in df_les]
+
+        vrms_struct = [df["STRUCTURAL_WW"] /df["RHO"] for df in df_les]
+        vrms_struct_dev = [df["STRUCTURAL_WW"] /df["RHO"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])/df["RHO"] for df in df_les]
+
+        wrms_struct = [df["STRUCTURAL_VV"]/df["RHO"] for df in df_les]
+        wrms_struct_dev = [df["STRUCTURAL_VV"]/df["RHO"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])/df["RHO"]  for df in df_les]
+
+        uv_struct = [df["STRUCTURAL_UW"] / df["RHO"] for df in df_les]
+    else: 
+        urms_struct = [df["STRUCTURAL_UU"] for df in df_les]
+        urms_struct_dev = [df["STRUCTURAL_UU"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"]) for df in df_les]
+
+        vrms_struct = [df["STRUCTURAL_WW"]  for df in df_les]
+        vrms_struct_dev = [df["STRUCTURAL_WW"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"]) for df in df_les]
+
+        wrms_struct = [df["STRUCTURAL_VV"] for df in df_les]
+        wrms_struct_dev = [df["STRUCTURAL_VV"] - 1/3 * (df["STRUCTURAL_UU"] + df["STRUCTURAL_VV"] + df["STRUCTURAL_WW"])  for df in df_les]
+
+        uv_struct = [df["STRUCTURAL_UW"] for df in df_les]
+
+
+    if pi_compressible:
+        u_theta_struct = [df["STRUCTURAL_USCALAR"]/df["RHO"] for df in df_les]
+        v_theta_struct = [df["STRUCTURAL_WSCALAR"]/df["RHO"]  for df in df_les]
+    else:
+        u_theta_struct = [df["STRUCTURAL_USCALAR"] for df in df_les]
+        v_theta_struct = [df["STRUCTURAL_WSCALAR"]  for df in df_les]
+
+    urms_fonc = [- 2 * df["NUTURB_XX_DUDX"] for df in df_les]
+    urms_fonc_dev  = [- 2 * (df["NUTURB_XX_DUDX"] - 1/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"])) for df in df_les]
+
+    # Tau_{y, y}
+    vrms_fonc = [- 2 * ( df["NUTURB_ZZ_DWDZ"]) for df in df_les]
+    vrms_fonc_dev  = [- 2 * ( df["NUTURB_ZZ_DWDZ"] - 1/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"])) for df in df_les]
+
+    # Tau_{z, z}
+    wrms_fonc = [- 2 *df["NUTURB_YY_DVDY"] for df in df_les]
+    wrms_fonc_dev  = [ - 2 *(df["NUTURB_YY_DVDY"] - 1/3 * (df["NUTURB_XX_DUDX"] + df["NUTURB_YY_DVDY"] + df["NUTURB_ZZ_DWDZ"]))  for df in df_les]
+
+    uv_fonc   = [- (df["NUTURB_XZ_DUDZ"] + df["NUTURB_XZ_DWDX"]) for df in df_les]
+    u_theta_fonc   =[- 2* df["KAPPATURB_X_DSCALARDX"]   for df in df_les]
+    v_theta_fonc   =[- 2 * df["KAPPATURB_Z_DSCALARDZ"]  for df in df_les]
+
+    struct = {"urms_dev": urms_struct_dev, "urms":urms_struct, "vrms": vrms_struct,"vrms_dev": vrms_struct_dev, "wrms":wrms_struct,
+              "wrms_dev":wrms_struct_dev , "uv": uv_struct, "u_theta": u_theta_struct, "v_theta": v_theta_struct}
+    fonc = {"urms_dev": urms_fonc_dev, "urms":urms_fonc, "vrms": vrms_fonc,"vrms_dev": vrms_fonc_dev, "wrms":wrms_fonc,
+              "wrms_dev":wrms_fonc_dev , "uv": uv_fonc, "u_theta": u_theta_fonc, "v_theta": v_theta_fonc}
+
+    return struct, fonc
+
+
+def adim_closure_terms(ref: RefData, tau: Dict[str, list[pd.DataFrame]], pi: Dict[str, list[pd.DataFrame]])->Dict[str, list[pd.DataFrame]]:
+    middles = [df["urms"][0].shape[0] //2 for df in tau]
+    tau_hot = dict()
+    tau_cold = dict()
+    
+    for key in ["urms_dev", "urms", "vrms_dev", "vrms", "wrms_dev", "wrms", "uv"]:
+        tau_hot[key] = [t.iloc[middle:]/(ref.utau["hot"]**2) for t, middle in zip(tau[key], middles)]
+        tau_cold[key] = [t.iloc[:middle]/(ref.utau["hot"]**2) for t, middle in zip(tau[key], middles)]
+
+        pi_hot[key] = [t.iloc[middle:]/(ref.utau["hot"]**2) for t, middle in zip(pi[key], middles)]
+        pi_cold[key] = [t.iloc[:middle]/(ref.utau["hot"]**2) for t, middle in zip(pi[key], middles)]
+
+    for key in ["u_theta", "v_theta"]:
+        tau_hot[key] = [t.iloc[middle:]/(ref.utau["hot"] * ref.sheer["thetatau"]) for t, middle in zip(tau[key], middles)]
+        tau_cold[key] = [t.iloc[:middle]/(ref.utau["hot"] * ref.sheer["thetatau"]) for t, middle in zip(tau[key], middles)]
+
+        pi_hot[key] = [t.iloc[middle:]/(ref.utau["hot"] * ref.sheer["thetatau"]) for t, middle in zip(pi[key], middles)]
+        pi_cold[key] = [t.iloc[:middle]/(ref.utau["hot"] * ref.sheer["thetatau"]) for t, middle in zip(pi[key], middles)]
+
+    tau_all = {"hot": tau_hot, "cold": tau_cold}
+    pi_all  = {"hot": pi_hot, "cold": pi_cold}
+    return tau_all, pi_all
+
+def adim_second_order_stats(ref: RefData, les: Dict[str, list[pd.DataFrame]], dns: Dict[str, list[pd.DataFrame]]) -> Dict[str, list[pd.DataFrame]]:
+    pass
 
 
 def mean_over_n_times(df: pd.DataFrame, n: int = 0):
