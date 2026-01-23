767f4c3cbcbc3e849224aeec1460853e047a6f75
Yanis ZATOUT <yanis.zatout@universite-paris-saclay.fr>
2026-01-23 15:00:17 +0100

    Impacting changes from dns.py into the LesData class

--- datahandling/les.py
+++ datahandling/les.py
@@ -26,49 +26,6 @@ class LesData(object):
     def load(self, ref_df=None, Cp=None):
         if not Cp:
             Cp = self.Cp
-        (
-            _,
-            self.utau,
-            self.thetatau,
-            self.retau,
-            self.rho_bulk,
-            self.u_bulk,
-            self.t_bulk,
-            self.re_bulk,
-            self.theta,
-            self.utau,
-            self.retau,
-            self.nusselt,
-            self.Cf,
-            self.y,
-            self.yplus,
-            self.df,
-            self.mu_bulk,
-            self.phi_tau,
-        ) = ref_values(self.ref_df, self.Cp, h=self.h, Tw=self.Tw)
-        self.ubulk = self.u_bulk
-        self.rebulk = self.re_bulk
-
-        self.wall = {"T": self.Tw}
-        self.sheer = self.tau = {
-            "utau": self.utau,
-            "retau": self.retau,
-            "Cf": self.Cf,
-            "thetatau": self.thetatau,
-            "phitau": self.phi_tau,
-        }
-        self.msh = {"y": self.y, "h": self.h}
-        self.T = self.thermal = {"Nu": self.nusselt, "nusselt": self.nusselt}
-        self.bulk = {
-            "rho_bulk": self.rho_bulk,
-            "u_bulk": self.u_bulk,
-            "t_bulk": self.t_bulk,
-            "re_bulk": self.re_bulk,
-            "mu_bulk": self.mu_bulk,
-        }
-        self.middle = len(self.y) // 2
-        self.Nusselt = self.Nu = self.nusselt
-        self.ny = self.y.shape[0]
 
     def __repr__(self):
         return (
